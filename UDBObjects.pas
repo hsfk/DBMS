6f88e486c569f5801fdc3ea1205c9275c12d184e
hsfk <hsfk@yandex.ru>
2016-04-16 14:41:08 +1000

    generic data class support + allignment added

--- UDBObjects.pas
+++ UDBObjects.pas
@@ -124,38 +124,31 @@ type
     property Data: TParam read GetData;
   end;
 
-  TGenericTable = specialize TGenericData<TDBField>;
+  _TTable = specialize _TGData<TDBField>;
 
-  TDBTable = class(TGenericTable)
+  TDBTable = class(_TTable)
   protected
     FQuery: ITableQuery;
     function CmpItemName(AField: TDBField; AName: string): boolean; override;
   public
-    property Back: TDBField read GetBack write SetBack;
-    property Front: TDBField read GetFront write SetFront;
     property Fields[AIndex: integer]: TDBField read GetItem write SetItem;
     property FieldsByName[AName: string]: TDBField read GetItemByName;
-    procedure SetItems(AFields: array of TDBField); override;
+    procedure SetFields(AFields: array of TDBField);
     constructor Create(AName, ANativeName: string; AIndex: integer = -1);
     procedure Assign(Table: TDBTable);
   published
-    property Count: integer read GetLength;
     property Query: ITableQuery read FQuery;
   end;
 
-  TGenericMetaData = specialize TGenericData<TDBTable>;
+  _TMetaData = specialize _TGData<TDBTable>;
 
-  TDBMetaData = class(TGenericMetaData)
+  TDBMetaData = class(_TMetaData)
   protected
     function CmpItemName(ATable: TDBTable; AName: string): boolean; override;
   public
-    property Back: TDBTable read GetBack write SetBack;
-    property Front: TDBTable read GetFront write SetFront;
     property Tables[AIndex: integer]: TDBTable read GetItem write SetItem;
     property TablesByName[AName: string]: TDBTable read GetItemByName;
-    procedure Add(Table: TDBTable); override;
-  published
-    property Count: integer read GetLength;
+    procedure SetTables(ATables: array of TDBTable);
   end;
 
 var
@@ -288,7 +281,7 @@ begin
   Exit(False);
 end;
 
-procedure TDBTable.SetItems(AFields: array of TDBField);
+procedure TDBTable.SetFields(AFields: array of TDBField);
 var
   i: integer;
 begin
@@ -309,6 +302,7 @@ end;
 procedure TDBTable.Assign(Table: TDBTable);
 begin
   inherited Assign(Table);
+  FQuery := Table.Query;
 end;
 
 function TDBMetaData.CmpItemName(ATable: TDBTable; AName: string): boolean;
@@ -319,61 +313,77 @@ begin
   Exit(False);
 end;
 
-procedure TDBMetaData.Add(Table: TDBTable);
+procedure TDBMetaData.SetTables(ATables: array of TDBTable);
+var
+  i: integer;
 begin
-  inherited Add(Table);
-  Back.Index := Count - 1;
+  inherited SetItems(ATables);
+  for i := 0 to Count - 1 do
+    Tables[i].Index := i;
 end;
 
 initialization
 
   DBData := TDBMetaData.Create('MetaData', 'MetaData');
+
   with DBData do begin
-    Add(TDBTable.Create( 'Расписание'     ,'Time_Table'    ));
-    Add(TDBTable.Create( 'Группы'         ,'Groups'        ));
-    Add(TDBTable.Create( 'Предметы'       ,'Lessons'       ));
-    Add(TDBTable.Create( 'Преподаватели'  ,'Teachers'      ));
-    Add(TDBTable.Create( 'Аудитории'      ,'Class_Rooms'   ));
-    Add(TDBTable.Create( 'Времена занятий','Lessons_Times' ));
-    Add(TDBTable.Create( 'Дни недели'     ,'Week_Days'     ));
-    Add(TDBTable.Create( 'Виды предметов' ,'Lessons_Types' ));
+
+    with TDBTable do begin
+      SetTables([
+         Create( 'Расписание'     ,'Time_Table'    )
+        ,Create( 'Группы'         ,'Groups'        )
+        ,Create( 'Предметы'       ,'Lessons'       )
+        ,Create( 'Преподаватели'  ,'Teachers'      )
+        ,Create( 'Аудитории'      ,'Class_Rooms'   )
+        ,Create( 'Времена занятий','Lessons_Times' )
+        ,Create( 'Дни недели'     ,'Week_Days'     )
+        ,Create( 'Виды предметов' ,'Lessons_Types' )
+      ]);
+    end;
 
     with TDBField do begin
-      TablesByName['Groups'].SetItems([
+      TablesByName
+      ['Groups'].SetFields([
          Create( 'ID'     ,'Id'   ,40  ,ftInteger )
         ,Create( 'Группа' ,'Name' ,100 ,ftString  )
       ]);
-      TablesByName['Lessons'].SetItems([
+      TablesByName
+      ['Lessons'].SetFields([
          Create( 'ID'      ,'Id'   ,40  ,ftInteger )
         ,Create( 'Предмет' ,'Name' ,300 ,ftString  )
       ]);
-      TablesByName['Teachers'].SetItems([
+      TablesByName
+      ['Teachers'].SetFields([
          Create( 'ID'       ,'Id'          ,40  ,ftInteger )
         ,Create( 'Фамилия'  ,'Last_Name'   ,100 ,ftString  )
         ,Create( 'Имя'      ,'First_Name'  ,100 ,ftString  )
         ,Create( 'Отчество' ,'Middle_Name' ,100 ,ftString  )
       ]);
-      TablesByName['Class_Rooms'].SetItems([
+      TablesByName
+      [ 'Class_Rooms'].SetFields([
          Create( 'ID'        ,'Id'   ,40  ,ftInteger )
         ,Create( 'Аудитория' ,'Name' ,100 ,ftString  )
       ]);
-      TablesByName['Lessons_Times'].SetItems([
+      TablesByName
+      ['Lessons_Times'].SetFields([
          Create( 'ID'     ,'Id'     ,40  ,ftInteger )
         ,Create( 'Начало' ,'Begin_' ,100 ,ftString  )
         ,Create( 'Конец'  ,'End_'   ,100 ,ftString  )
       ]);
-      TablesByName['Week_Days'].SetItems([
+      TablesByName
+      ['Week_Days'].SetFields([
          Create( 'ID'          ,'Id'   ,40  ,ftInteger )
         ,Create( 'День недели' ,'Name' ,100 ,ftString  )
       ]);
-      TablesByName['Lessons_Types'].SetItems([
+      TablesByName
+      ['Lessons_Types'].SetFields([
          Create( 'ID'  ,'Id'   ,40  ,ftInteger )
         ,Create( 'Тип' ,'Name' ,130 ,ftString  )
       ]);
     end;
 
     With TDBReferenceField do begin
-      TablesByName['Time_Table'].SetItems([
+      TablesByName['Time_Table'].SetFields([
         TDBField.Create('ID','Id', 40, ftInteger),
         Create('Time_Table' ,'Lesson_Id'      ,TablesByName['Lessons'],       'Id', 'Name'),
         Create('Time_Table' ,'Lesson_Type_Id' ,TablesByName['Lessons_Types'], 'Id', 'Name'),
@@ -387,6 +397,7 @@ initialization
         Create('Time_Table' ,'Lesson_Time_Id' ,TablesByName['Lessons_Times'], 'Id', 'End_')
       ]);
     end;
+
   end;
 
 end.
