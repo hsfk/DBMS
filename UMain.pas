8154f663d915ee3f234637a2eda1d00f9efbb3e9
hsfk <hsfk@yandex.ru>
2016-04-21 16:23:09 +1000

    added DBConnection field

--- UMain.pas
+++ UMain.pas
@@ -6,38 +6,30 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-  Menus, ComCtrls, UDb, UDirectory, UAbout, UNotifications, UDBObjects, USchedule;
+  Menus, ComCtrls, UDBConnection, UDirectory, UAbout, UNotifications,
+  UDBObjects, USchedule;
 
 type
 
-  { TMainForm }
-
   TMainForm = class(TForm)
   private
-    FIsConnected: boolean;
     FNotifications: TSubscriber;
+    FDBConnection: TDBConnection;
     procedure CreateDirectoryForm(Table: TDBTable);
+    procedure LoadDirectoryMenyItems;
   published
-    MainMenu: TMainMenu;
-    FileMenu: TMenuItem;
-    DirectoryMenu: TMenuItem;
-    AboutMenu: TMenuItem;
-    Groups: TMenuItem;
-    Lessons: TMenuItem;
-    ClassRooms: TMenuItem;
-    LessonTypes: TMenuItem;
-    LessonTimes: TMenuItem;
-    OpenMenuItem: TMenuItem;
+    FMainMenu: TMainMenu;
+    FFileMenu: TMenuItem;
+    FDirectoryMenu: TMenuItem;
+    FAboutMenu: TMenuItem;
+    FOpenMenuItem: TMenuItem;
     FStatusBar: TStatusBar;
-    WeekDays: TMenuItem;
-    Teachers: TMenuItem;
-    TimeTable: TMenuItem;
-    ScheduleMenu: TMenuItem;
-    procedure ScheduleMenuClick(Sender: TObject);
-    procedure AboutMenuClick(Sender: TObject);
+    FScheduleMenu: TMenuItem;
+    procedure FScheduleMenuClick(Sender: TObject);
+    procedure FAboutMenuClick(Sender: TObject);
     procedure DirectoryMenuItemClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure OpenMenuItemClick(Sender: TObject);
+    procedure FOpenMenuItemClick(Sender: TObject);
   end;
 
 var
@@ -46,69 +38,90 @@ var
 implementation
 
 {$R *.lfm}
-{ TMainForm }
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
-  MainForm.Constraints.MinHeight := MainForm.Height;
-  MainForm.Constraints.MinWidth := MainForm.Width;
-  MainForm.Caption := APP_NAME + CURRENT_VERSION;
-  FIsConnected := True;
-  FStatusBar.SimpleText := DbConnection.CurrentConnection;
+  FDBConnection := TDBConnection.Create;
+  FDBConnection.Connect('DataBase.fdb', 'sysdba', 'masterkey');
+  Constraints.MinHeight := 40;
+  Constraints.MinWidth := Width;
+  Caption := APP_CAPTION;
+  FStatusBar.SimpleText := FDBConnection.CurrentConnection;
   FNotifications := TSubscriber.Create(True);
+  LoadDirectoryMenyItems;
 end;
 
-procedure TMainForm.ScheduleMenuClick(Sender: TObject);
+procedure TMainForm.FScheduleMenuClick(Sender: TObject);
 var
   Schedule: TSchedule;
+  NClass: TNClass;
 begin
-  Application.CreateForm(TSchedule, Schedule);
-  Schedule.Show;
+  if FDBConnection.Connected then begin
+    SetLength(NClass, 1);
+    NClass[0] := 0;
+    Application.CreateForm(TSchedule, Schedule);
+    Schedule.InitConnection(FDBConnection);
+    Schedule.Load(NClass, DBData.TablesByName['Time_Table']);
+    Schedule.Show;
+  end
+  else
+    ShowMessage('Необходимо подключение к базе данных.');
 end;
 
 procedure TMainForm.CreateDirectoryForm(Table: TDBTable);
 var
   Directory: TDirectory;
-  NotificationClass: TNClass;
+  NClass: TNClass;
   i: integer;
 begin
-  SetLength(NotificationClass, 0);
+  SetLength(NClass, 0);
   for i := 0 to Table.Count - 1 do begin
-    SetLength(NotificationClass, Length(NotificationClass) + 1);
-    NotificationClass[High(NotificationClass)] := Table.Fields[i].ParentTable.Index;
+    SetLength(NClass, Length(NClass) + 1);
+    NClass[High(NClass)] := Table.Fields[i].ParentTable.Index;
   end;
   Application.CreateForm(TDirectory, Directory);
-  Directory.InitConnection(DbConnection);
-  Directory.Load(NotificationClass, Table);
+  Directory.InitConnection(FDBConnection);
+  Directory.Load(NClass, Table);
   FNotifications.Subscribe(Directory.ThisSubscriber);
   Directory.Show;
 end;
 
-procedure TMainForm.OpenMenuItemClick(Sender: TObject);
+procedure TMainForm.LoadDirectoryMenyItems;
+var
+  i: integer;
+  MenuItem: TMenuItem;
+begin
+  for i := 0 to DBData.Count - 1 do begin
+    MenuItem := TMenuItem.Create(FDirectoryMenu);
+    MenuItem.Tag := i;
+    MenuItem.OnClick := @DirectoryMenuItemClick;
+    MenuItem.Caption := DBData.Tables[i].Name;
+    FDirectoryMenu.Add(MenuItem);
+  end;
+end;
+
+procedure TMainForm.FOpenMenuItemClick(Sender: TObject);
 var
   OpenDialog: TOpenDialog;
 begin
   OpenDialog := TOpenDialog.Create(Self);
   OpenDialog.Filter := 'Firebird Database|*.fdb|All Files|*.*';
   if OpenDialog.Execute then begin
-    FIsConnected := True;
-    DbConnection.Connect(OpenDialog.FileName, 'sysdba', 'masterkey');
-    FStatusBar.SimpleText := DbConnection.CurrentConnection;
+    FDBConnection.Connect(OpenDialog.FileName, 'sysdba', 'masterkey');
+    FStatusBar.SimpleText := FDBConnection.CurrentConnection;
   end;
   OpenDialog.Free;
 end;
 
 procedure TMainForm.DirectoryMenuItemClick(Sender: TObject);
 begin
-  if FIsConnected = True then begin
-    CreateDirectoryForm(DBData.Tables[TMenuItem(Sender).Tag]);
-  end
-  else begin
+  if FDBConnection.Connected then
+    CreateDirectoryForm(DBData.Tables[TMenuItem(Sender).Tag])
+  else
     ShowMessage('Необходимо подключение к базе данных.');
-  end;
 end;
 
-procedure TMainForm.AboutMenuClick(Sender: TObject);
+procedure TMainForm.FAboutMenuClick(Sender: TObject);
 var
   About: TAboutForm;
 begin
