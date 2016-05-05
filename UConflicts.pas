f26d72a8dd99a7367a0bed0fe7fdbcdb2039b8a4
hsfk <hsfk@yandex.ru>
2016-05-06 06:06:59 +1000

    conflicts refactored

--- UConflicts.pas
+++ UConflicts.pas
@@ -12,8 +12,7 @@ type
 
   TConflictType = class;
   TConflictPanel = class;
-  TDataTuple = specialize TVector<TStringM>;
-  TConflictTypes = specialize TObjVector<TConflictType>;
+  TDataTuple = specialize TVector<TStringV>;
   TData = specialize TVector<TDataTuple>;
   TEditEvent = procedure(Conflict: TConflictPanel) of object;
 
@@ -29,26 +28,28 @@ type
     FEQRecIDs: TIntegerV;
     FNEQRecIDs: TIntegerV;
     FData: TData;
+    procedure ShowData(AData: TData);
+    procedure ShowTuple(ATuple: TDataTuple);
+    procedure DeleteOneElTuples(AData: TData);
+    procedure FreeData(AData: TData);
+    // separates data tuple into set of vectors, each vector represents
+    // set of data with the same equal records
+    function SeparateEQ(ADataTuple: TDataTuple; EQRec: integer): TData;
+    function SeparateEQ(AData: TData; EQRec: integer): TData;
+    // deletes data tuple item if its NEQRec record occures once
+    function DeleteNEQ(ADataTuple: TDataTuple; NEQRec: integer): TData;
+    function DeleteNEQ(AData: TData; NEQRec: integer): TData;
   public
     constructor Create(AName: string);
     destructor Destroy; override;
-    procedure AddConditions(EQRecIDs, NEQRecIDs: TIntegerV);
-    procedure ShowData(AData: TData);
-    procedure ShowTuple(ATuple: TDataTuple);
     procedure SelectConflicted(DataTuple: TDataTuple);
-    procedure DeleteOneElTuples(Data: TData);
+    procedure AddConditions(EQRecIDs, NEQRecIDs: TIntegerV);
     function ResultTuple: TResultTuple;
-    // separates data tuple into set of vectors, each vector represents
-    // set of data with the same equal records
-    function SeparateEQ(Data: TDataTuple; EQRec: integer): TData;
-    function SeparateEQ(Data: TData; EQRec: integer): TData;
-    // deletes data tuple item if its NEQRec record occures once
-    function DeleteNEQ(Data: TDataTuple; NEQRec: integer): TData;
-    function DeleteNEQ(Data: TData; NEQRec: integer): TData;
   published
     property EQRecIDs: TIntegerV read FEQRecIDs write FEQRecIDs;
     property NEQRecIDs: TIntegerV read FNEQRecIDs write FNEQRecIDs;
     property Name: string read FName;
+    property Data: TData read FData;
   end;
 
   TConflictPanel = class(TCustomControl)
@@ -57,6 +58,7 @@ type
     FEditBtn: TButton;
     FEdit: TEdit;
     FOnEdit: TEditEvent;
+    FTreeViewVisRec: integer;
     procedure EditClick(Sender: TObject);
   public
     constructor Create(AName: string);
@@ -65,6 +67,7 @@ type
   published
     property Conflict: TConflictType read FConflict write FConflict;
     property Edit: TEdit read FEdit write FEdit;
+    property TreeViewVisibleRec: integer read FTreeViewVisRec write FTreeViewVisRec;
   end;
 
   TCustomConflictPanels = specialize TCustomControls<TConflictPanel>;
@@ -75,8 +78,9 @@ type
     procedure SetEvent(Event: TEditEvent);
   public
     constructor Create(AParent: TWinControl; ATop, ALeft: integer);
-    procedure AddConflict(AName: string; EQRecIDs, NEQRecIDs: TIntegerV);
-    procedure AddConflict(Conflict: TConflictType);
+    procedure AddConflict(AName: string; EQRecIDs, NEQRecIDs: TIntegerV;
+      ATreeViewVisRec: integer = 0);
+    function AddConflict(Conflict: TConflictType): TConflictPanel;
     procedure AnalyzeData(Data: TDataTuple);
     function GetResult: TResultTuple;
   published
@@ -99,31 +103,32 @@ begin
   FOnEdit := nil;
   inherited Create(AParent, ATop, ALeft);
   with TIntegerV do begin
-    AddConflict('Разрыв группы', Create([9, 8, 6]), Create([7]));
-    AddConflict('Разрыв преподавателя', Create([3, 8, 9]), Create([7]));
-    AddConflict('Больше 1 преподавателя в аудитории', Create([7, 8, 9, 6]), Create([3]));
-    AddConflict('Больше 1 группы в аудитории', Create([7, 8, 9]), Create([6]));
+    AddConflict('Разрыв группы', Create([9, 8, 6]), Create([7]), 6);
+    AddConflict('Разрыв преподавателя', Create([3, 8, 9]), Create([7]), 3);
+    AddConflict('Больше 1 преподавателя в аудитории', Create([7, 8, 9, 6]),
+      Create([3]), 3);
+    AddConflict('Больше 1 группы в аудитории', Create([7, 8, 9]), Create([6]), 6);
   end;
 end;
 
-procedure TConflictPanels.AddConflict(AName: string; EQRecIDs, NEQRecIDs: TIntegerV);
+procedure TConflictPanels.AddConflict(AName: string; EQRecIDs, NEQRecIDs: TIntegerV;
+  ATreeViewVisRec: integer = 0);
 var
   Panel: TConflictPanel;
 begin
   Panel := TConflictPanel.Create(AName, FParent, FTop, FLeft);
   Panel.Conflict.AddConditions(EQRecIDs, NEQRecIDs);
   Panel.FOnEdit := FOnEdit;
+  Panel.TreeViewVisibleRec := ATreeViewVisRec;
   AddControlPanel(Panel);
 end;
 
-procedure TConflictPanels.AddConflict(Conflict: TConflictType);
-var
-  Panel: TConflictPanel;
+function TConflictPanels.AddConflict(Conflict: TConflictType): TConflictPanel;
 begin
-  Panel := TConflictPanel.Create(Conflict.Name, FParent, FTop, FLeft);
-  Panel.Conflict := Conflict;
-  Panel.FOnEdit := FOnEdit;
-  AddControlPanel(Panel);
+  Result := TConflictPanel.Create(Conflict.Name, FParent, FTop, FLeft);
+  Result.Conflict := Conflict;
+  Result.FOnEdit := FOnEdit;
+  AddControlPanel(Result);
 end;
 
 procedure TConflictPanels.AnalyzeData(Data: TDataTuple);
@@ -132,15 +137,16 @@ var
 begin
   for i := 0 to Size - 1 do
     Items[i].Conflict.SelectConflicted(Data);
+  Data.Free;
 end;
 
 function TConflictPanels.GetResult: TResultTuple;
 var
-  Temp: TResultTuple;
+  Temp: TResultTuple = nil;
   i: integer;
   j: integer;
 begin
-  SetLength(Result, 0);
+  Result := nil;
   for i := 0 to Size - 1 do begin
     Temp := Items[i].Conflict.ResultTuple;
     if Temp <> nil then
@@ -173,19 +179,29 @@ begin
   FNEQRecIDs := NEQRecIDs;
 end;
 
-procedure TConflictType.DeleteOneElTuples(Data: TData);
+procedure TConflictType.DeleteOneElTuples(AData: TData);
 var
   i: integer = 0;
   Size: integer;
 begin
-  Size := Data.Size;
+  Size := AData.Size;
   while i < Size do
-    if Data[i].Size <= 1 then begin
-      Data.DeleteInd(i);
+    if AData[i].Size <= 1 then begin
+      AData[i].Free;
+      AData.DeleteInd(i);
       Size -= 1;
     end
     else
       i += 1;
+end;
+
+procedure TConflictType.FreeData(AData: TData);
+var
+  i: integer;
+begin
+  for i := 0 to AData.Size - 1 do
+    AData[i].Free;
+  AData.Free;
 end;
 
 function TConflictType.ResultTuple: TResultTuple;
@@ -195,11 +211,11 @@ var
 begin
   if FData = nil then
     Exit(nil);
-  SetLength(Result, 0);
+  Result := nil;
   for i := 0 to FData.Size - 1 do
     for j := 0 to FData[i].Size - 1 do begin
       SetLength(Result, Length(Result) + 1);
-      Result[High(Result)].RecID := StrToInt(FData[i][j][0, 0]);
+      Result[High(Result)].RecID := StrToInt(FData[i][j][0]);
       Result[High(Result)].ConflictType := Self;
     end;
 end;
@@ -210,6 +226,8 @@ var
 begin
   if DataTuple.Size = 0 then
     Exit;
+  if FData <> nil then
+    FreeData(FData);
   FData := SeparateEQ(DataTuple, FEQRecIDs[0]);
   for i := 0 to FEQRecIDs.Size - 1 do
     FData := SeparateEQ(FData, FEQRecIDs[i]);
@@ -220,80 +238,79 @@ begin
   end;
 end;
 
-function TConflictType.SeparateEQ(Data: TDataTuple; EQRec: integer): TData;
+function TConflictType.SeparateEQ(ADataTuple: TDataTuple; EQRec: integer): TData;
 var
   i: integer = 1;
   j: integer;
   Index: integer;
 begin
-  Result := TData.Create(TDataTuple.Create(Data[0]));
-  for i := 1 to Data.Size - 1 do begin
+  Result := TData.Create(TDataTuple.Create(ADataTuple[0]));
+  for i := 1 to ADataTuple.Size - 1 do begin
     Index := -1;
     for j := 0 to Result.Size - 1 do
-      if Result[j][0][0, EQRec] = Data[i][0, EQRec] then begin
+      if Result[j][0][EQRec] = ADataTuple[i][EQRec] then begin
         Index := j;
         break;
       end;
     if Index <> -1 then
-      Result[Index].PushBack(Data[i])
+      Result[Index].PushBack(ADataTuple[i])
     else
-      Result.PushBack(TDataTuple.Create(Data[i]));
+      Result.PushBack(TDataTuple.Create(ADataTuple[i]));
   end;
   DeleteOneElTuples(Result);
   Exit(Result);
 end;
 
-function TConflictType.SeparateEQ(Data: TData; EQRec: integer): TData;
+function TConflictType.SeparateEQ(AData: TData; EQRec: integer): TData;
 var
   i: integer = 0;
   j: integer;
   Buffer: TData;
 begin
   Result := TData.Create;
-  for i := 0 to Data.Size - 1 do begin
-    Buffer := SeparateEQ(Data[i], EQRec);
+  for i := 0 to AData.Size - 1 do begin
+    Buffer := SeparateEQ(AData[i], EQRec);
     for j := 0 to Buffer.Size - 1 do
       Result.PushBack(Buffer[j]);
     Buffer.Free;
   end;
-  Data.Free;
+  FreeData(AData);
 end;
 
-function TConflictType.DeleteNEQ(Data: TDataTuple; NEQRec: integer): TData;
+function TConflictType.DeleteNEQ(ADataTuple: TDataTuple; NEQRec: integer): TData;
 var
   i: integer = 1;
   j: integer;
   Counter: integer;
 begin
   Result := TData.Create(TDataTuple.Create);
-  for i := 0 to Data.Size - 1 do begin
+  for i := 0 to ADataTuple.Size - 1 do begin
     Counter := 0;
-    for j := 0 to Data.Size - 1 do
-      if (Data[i][0, NEQRec] = Data[j][0, NEQRec]) then begin
+    for j := 0 to ADataTuple.Size - 1 do
+      if (ADataTuple[i][NEQRec] = ADataTuple[j][NEQRec]) then begin
         Counter += 1;
         if Counter > 1 then
           break;
       end;
     if Counter = 1 then
-      Result[0].PushBack(Data[i]);
+      Result[0].PushBack(ADataTuple[i]);
   end;
   if Result[0].Size = 0 then begin
-    Data.Free;
-    Result.Free;
+    FreeData(Result);
     Exit(nil);
   end;
   Exit(Result);
 end;
 
-function TConflictType.DeleteNEQ(Data: TData; NEQRec: integer): TData;
+function TConflictType.DeleteNEQ(AData: TData; NEQRec: integer): TData;
 var
   i: integer;
   j: integer;
   Buffer: TData;
 begin
   Result := TData.Create;
-  for i := 0 to Data.Size - 1 do begin
-    Buffer := DeleteNEQ(Data[i], NEQRec);
+  for i := 0 to AData.Size - 1 do begin
+    Buffer := DeleteNEQ(AData[i], NEQRec);
     if Buffer <> nil then
       for j := 0 to Buffer.Size - 1 do
         Result.PushBack(Buffer[j]);
@@ -303,20 +320,22 @@ begin
     Result.Free;
     Exit(nil);
   end;
+  FreeData(AData);
+  DeleteOneElTuples(Result);
 end;
 
 procedure TConflictType.ShowTuple(ATuple: TDataTuple);
 var
   i: integer;
   j: integer;
-  TempM: TStringM;
+  TempM: TStringV;
   Text: string;
 begin
   Text := '';
   for i := 0 to ATuple.Size - 1 do begin
     TempM := ATuple[i];
-    for j := 0 to TempM.Height - 1 do begin
-      Text += TempM[0, j];
+    for j := 0 to TempM.Size - 1 do begin
+      Text += TempM[j];
       Text += #13#10;
     end;
     Text += #13#10;
@@ -342,6 +361,7 @@ constructor TConflictPanel.Create(AName: string);
 begin
   FConflict := TConflictType.Create(AName);
   FOnEdit := nil;
+  FTreeViewVisRec := 0;
 end;
 
 constructor TConflictPanel.Create(AName: string; AParent: TWinControl;
